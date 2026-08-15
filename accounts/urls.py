45c878ccc2872fddae9e4b1f0bb41e19adbc7c85
salmankhan.dev01 <jickhirisalmankhan22@gmail.com>
2026-08-15 09:46:16 +0530

    Prepare DonateHub for Render deployment

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
+    path(
+        "register/",
+        views.register,
+        name="register"
+    ),
     path(
         "login/",
         views.user_login,
