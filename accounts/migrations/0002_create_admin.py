6611c6bd11316461193f9b28fa6705bb96490214
salmankhan.dev01 <jickhirisalmankhan22@gmail.com>
2026-08-15 11:34:40 +0530

    update Super

--- accounts/migrations/0002_create_admin.py
+++ accounts/migrations/0002_create_admin.py
@@ -1,20 +1,19 @@
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
 
 
 def create_admin_user(apps, schema_editor):
     User = apps.get_model("accounts", "User")
 
     if not User.objects.filter(username="admin").exists():
-        user = User.objects.create(
+        User.objects.create(
             username="admin",
             email="admin@gmail.com",
+            password=make_password("12341234"),
             is_staff=True,
             is_superuser=True,
             is_active=True,
         )
-
-        user.set_password("12341234")
-        user.save()
 
 
 def remove_admin_user(apps, schema_editor):
